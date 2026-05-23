388c798d706e1b90ea2d06d3886fc9523444cc2a
Atnan Tas <atnan.tas.pro@gmail.com>
2026-05-23 18:47:25 +0200

    Ajout de nouveaux projets et logos dans le composant projectsData

--- src/app/components/figma/ImageWithFallback.tsx
+++ src/app/components/figma/ImageWithFallback.tsx
@@ -5,7 +5,7 @@ const ERROR_IMG_SRC =
 
 export function ImageWithFallback(props: React.ImgHTMLAttributes<HTMLImageElement>) {
   const [didError, setDidError] = useState(false)
-
+ 
   const handleError = () => {
     setDidError(true)
   }
